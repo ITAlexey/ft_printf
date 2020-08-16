bea8482b3496af6c3ac5b297b17d2838b80d32ca
Alexey Artemyev <alexey.artemyev96@gmail.com>
2020-08-16 23:03:38 +0600

    continue amending output according to flags and width

--- src/flags.c
+++ src/flags.c
@@ -3,42 +3,6 @@
 //
 
 #include "ft_printf.h"
-#define LEFT 0
-#define RIGHT 1
-
-static void		alignment_by_zeros(t_data_format *data, char *arg, int arg_len, int diff)
-{
-		*(data->output) = '-';
-		ft_strncpy(data->output + 1 + diff, arg + 1, arg_len);
-		ft_memset((char*)(data->output + 1), ZERO, diff);
-}
-
-static void 	align_output(t_data_format *data, char *arg, char *direction, char to_fill)
-{
-	int 	arg_len;
-	int		diff;
-	int 	offset;
-	short 	side;
-
-	arg_len = ft_strlen(arg);
-	diff = arg_len - data->numeric_value_of_width;
-	if (diff >= 0)
-		data->output = ft_strsub(arg, 0, arg_len);
-	else
-	{
-		diff = ABC(diff);
-		data->output = ft_strnew(data->numeric_value_of_width);
-		side = (ft_strequ("left", direction) == 1) ? LEFT : RIGHT;
-		offset = side == LEFT ? 0 : diff;
-		if (*arg != '-' || to_fill == SPACE)
-		{
-			ft_strncpy(data->output + offset, arg, arg_len);
-			ft_memset((side == LEFT ? (char*)(data->output + arg_len) : (char*)(data->output)), to_fill, diff);
-		}
-		else
-			alignment_by_zeros(data, arg, arg_len - 1, diff);
-	}
-}
 
 void 		add_sign(t_data_format *data, char *sign)
 {
@@ -58,7 +22,25 @@ void		process_flag(t_data_format *data, t_flag *flag, char *arg)
 	width = ft_atoi(data->width);
 	if (len >= width)
 	{
-		if ((flag->pos + flag->space) != 0)
+		if ((flag->pos + flag->space) != 0 && *(data->argument) != '-')
 			add_sign(data, flag->pos == TRUE ? "+" : " ");
+	}
+	else
+	{
+		char *tmp;
+
+		tmp = data->argument;
+		data->argument = ft_strnew(width);
+		if (flag->neg == TRUE)
+		{
+			if (*(data->argument) == '-')
+				ft_memset((char*)(ft_strcpy(data->argument, tmp) + len), SPACE, width - len);
+			
+		}
+		else
+		{
+
+		}
+		free(tmp);
 	}
 }
